b896fd5c1117ee5024dc143f61f3f7c96a410474
yasmimcosm <yasmimcosm@gmail.com>
2026-01-21 19:46:26 -0300

    style: melhora responsividade para celulares

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const swiper = new Swiper('.best-sellers__slider', {
 
     breakpoints: {
         0: {
-            slidesPerView: 2,
+            slidesPerView: 1,
         },
         768: {
             slidesPerView: 2,
